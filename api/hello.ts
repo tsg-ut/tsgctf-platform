8d458b5f571cf2dda171a4be26bb36941bdd0595
Koki Takahashi <hakatasiloving@gmail.com>
2023-11-23 16:32:27 +0000

    Implement Vercel Postgres example

--- api/hello.ts
+++ api/hello.ts
@@ -1,7 +1,18 @@
 import type {VercelRequest, VercelResponse} from '@vercel/node';
+import {sql} from '@vercel/postgres';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+	const incrementResult = await sql`
+		UPDATE counters SET count = count + 1;
+	`;
+
+	const {rows: [{count}]} = await sql<{count: number}>`
+		SELECT count FROM counters;
+	`;
+
 	res.status(200).json({
+		incrementResult,
 		message: 'Hello world',
+		count,
 	});
 }
