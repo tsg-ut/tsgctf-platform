163bd022d07fb06e3d0b726494fd0dc8a212173c
Koki Takahashi <hakatasiloving@gmail.com>
2023-11-23 16:21:08 +0000

    Add vercel configurations

---
+++ api/hello.ts
@@ -0,0 +1,7 @@
+import type {VercelRequest, VercelResponse} from '@vercel/node';
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+	res.status(200).json({
+		message: 'Hello world',
+	});
+}
