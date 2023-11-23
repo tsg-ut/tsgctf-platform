68e556c24f9996d98490cefc2bd58e0dfe0a0b0c
Koki Takahashi <hakatasiloving@gmail.com>
2023-11-23 19:38:14 +0000

    Remove references to session key

--- api/challenges/poll.ts
+++ api/challenges/poll.ts
@@ -28,7 +28,6 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
 		credentials: {
 			accessKeyId: process.env.AWS_ACCESS_KEY_ID,
 			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-			sessionToken: process.env.AWS_SESSION_TOKEN,
 		},
 	});
 
